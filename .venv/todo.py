fc0f51c8f346b248ad49286b7f5501c3791cbb0a
Szymon Stefański <szymonstefanski1@gmail.com>
2025-01-31 22:35:32 +0100

    Add untracked files

---
+++ .venv/todo.py
@@ -0,0 +1,40 @@
+todos = []
+
+while True:
+    try:
+        answer = (input("What do you want to do?: " + "\n"
+                        + "Show" + "\n"
+                        + "Add" + "\n"
+                        + "Modify" + "\n"
+                        + "Delete" + "\n"
+                        + "Exit" + "\n")).capitalize()
+
+        if answer.startswith("Show"):
+            for todo in todos:
+                print(todo)
+
+            print("\n")
+
+        elif answer.startswith("Add"):
+            add = input("Please enter your task:").capitalize()
+            todos.append(add)
+            print(add + " was added to the list." + "\n")
+
+        elif answer.startswith("Modify"):
+            modify = input("Which task do you want to modify?:").capitalize()
+            todos.pop(modify)
+
+            newTodo = input("Please enter a new task:").capitalize()
+            todos.insert(modify, newTodo)
+            print(modify + " was modified." + "\n")
+
+        elif answer.startswith("Delete"):
+            delete = input("Which task do you want to delete?:").capitalize()
+            todos.remove(delete)
+            print(delete + " was deleted from the list." + "\n")
+
+        elif answer.startswith("Exit"):
+            break
+
+    except ValueError:
+        print("Invalid input")
