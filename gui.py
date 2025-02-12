1e2c5f5f9e3154c2e7ff821a178bfa3cda580626
Szymon Stefański <szymonstefanski1@gmail.com>
2025-02-12 23:40:00 +0100

    Add more buttons to GUI

--- gui.py
+++ gui.py
@@ -2,8 +2,33 @@ import FreeSimpleGUI
 
 label = FreeSimpleGUI.Text("What you want to do?")
 input_box = FreeSimpleGUI.InputText()
-add_button = FreeSimpleGUI.Button("Add")
 
-window = FreeSimpleGUI.Window('To_do', layout=[[label], [input_box], [add_button]])
-window.read()
-window.close()
+show_button = FreeSimpleGUI.Button("Show")
+add_button = FreeSimpleGUI.Button("Add")
+modify_button = FreeSimpleGUI.Button("Modify")
+delete_button = FreeSimpleGUI.Button("Delete")
+exit_button = FreeSimpleGUI.Button("Exit")
+
+window = FreeSimpleGUI.Window('To_do',
+                              layout=[[label], [input_box],
+                                      [show_button],
+                                      [add_button],
+                                      [modify_button],
+                                      [delete_button],
+                                      [exit_button]],
+                              font=('Arial', 12))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    '''match event:
+        case "Show":
+
+        case "Add":
+
+        case "Modify":
+
+        case "Delete":
+
+        case "Exit":
+        '''
