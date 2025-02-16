bfd82adc06a7b4af94ba82c8f6ae69ba92ae8fd7
Szymon Stefański <szymonstefanski1@gmail.com>
2025-02-16 23:28:28 +0100

    Add option to add and remove tasks from todo list by using web app

--- web.py
+++ web.py
@@ -1,7 +1,17 @@
+from operator import index
+
 import streamlit
+from streamlit import checkbox
+
 import options
 
+
 tasks = options.show()
+
+def add_task():
+    task = streamlit.session_state["new_task"]
+    tasks.append(task)
+    options.add(task)
 
 streamlit.title("Todo:")
 streamlit.subheader("This is my app.")
@@ -9,6 +19,12 @@ streamlit.write("This app is to increase your productivity.")
 
 
 for task in tasks:
-    streamlit.checkbox(task)
+    checkbox = streamlit.checkbox(task, key=task)
+    if checkbox:
+        tasks.remove(task)
+        options.delete(task)
+        streamlit.session_state.pop(task, None)
+        streamlit.rerun()
 
-streamlit.text_input(label="", placeholder="Enter your task:")
+streamlit.text_input(label="New Task", placeholder="Enter your task:",
+                     on_change=add_task, key='new_task', label_visibility="hidden")
